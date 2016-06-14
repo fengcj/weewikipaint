3f347b9cc64b977f26059a530da1c6f6e4196cf0
fengcj <fcjljy@gmail.com>
2016-06-14 20:37:21 +0800

    add lint for js static check

--- jakefile.js
+++ jakefile.js
@@ -1,6 +1,49 @@
+/*global desc, task, jake, fail, complete */
 (function(){
     'use strict';
-    task('example',function(){
-       console.log('done');
+
+
+    desc('default task');
+    task('default',['lint'],function(){
+
     });
+
+    desc('Lint everything');
+    task('lint',function(){
+        var lint = require("./build/lint/lint_runner.js");
+
+        var files = new jake.FileList();
+        files.include("**/*.js");
+        files.exclude("node_modules");
+        var options = nodeLintOptions();
+
+        lint.validateFileList(files.toArray(), options, {});
+
+    });
+
+
+
+
+
+    function nodeLintOptions() {
+        return {
+            bitwise:true,
+            curly:false,
+            eqeqeq:true,
+            forin:true,
+            immed:true,
+            latedef:false,
+            newcap:true,
+            noarg:true,
+            noempty:true,
+            nonew:true,
+            regexp:true,
+            undef:true,
+            strict:true,
+            trailing:true,
+            node:true
+        };
+    }
+
+
 }());
