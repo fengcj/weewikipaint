b321613a19ebd2c07f0c05023369a2db88312338
fengcj <fcjljy@gmail.com>
2016-09-26 18:52:25 +0800

    add read note

---
+++ changBackgroundColor.js
@@ -0,0 +1,16 @@
+document.body.style.backgroundColor = "rgb(199,237,204)";
+document.getElementsByClassName("rich_media_inner")[0]
+document.getElementsByClassName("rich_media_inner")[0].style.backgroundColor = "rgb(199,237,204)";
+document.getElementsByClassName("rich_media_area_primary")[0].style.backgroundColor = "rgb(199,237,204)";
+
+document.getElementsByClassName("qr_code_pc")[0].style.display = "none";
+
+
+
+Array.prototype.slice.call(document.getElementsByTagName("div")).forEach(function(element){
+	element.style.backgroundColor="rgb(199,237,204)";
+});;
+
+
+document.getElementById("content").style.backgroundColor = "rgb(199,237,204)";
+	
